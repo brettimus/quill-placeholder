f6b30ae09e21fd43e93e1932f27d1cc48daa3b0e
brettimus <brbeut@gmail.com>
2015-09-29 13:54:15 -0700

    remove default styles if format is not whitelisted. warn user that they are not getting default styles

--- placeholder.js
+++ placeholder.js
@@ -4,7 +4,7 @@
         this.quill    = quill;
         this.options  = options;
         this.text     = options.text  || "";
-        this.style    = options.style || { 'color': '#a9a9a9' };
+        this.style    = this.extend(this.getStyleDefaults(), options.style);
 
         var handler   = this.placeholderHandler.bind(this);
 
@@ -42,6 +42,40 @@
         }
     };
 
+    Placeholder.prototype.getStyleDefaults = function() {
+        var defaults = {
+                color: '#a9a9a9',
+            },
+            formats = this.quill.options.formats;
+
+        Object.keys(defaults).forEach(function(defaultFormat) {
+            var isValidFormat = formats.some(function(whitelistedFormat) {
+                return defaultFormat === whitelistedFormat;
+            });
+            if (!isValidFormat) {
+                printFormatWarning(defaultFormat);
+                delete defaults[defaultFormat];
+            }
+        });
+
+        return defaults;
+
+        function printFormatWarning(formatName) {
+            var msg = "[quill-placeholder] Warning!\n\nDefault format '" +
+                        formatName +
+                        "' is not whitelisted by your quill instance.\nPass '" +
+                        formatName +
+                        "' into the 'formats' array of your quill config to apply default styles.";
+            console.log(msg);
+        }
+    };
+
+    Placeholder.prototype.extend = function(o1, o2) {
+        for (var prop in o2)
+            if (o2.hasOwnProperty(prop))
+                o1[prop] = o2[prop];
+        return o1;
+    };
 
 
     Placeholder.prototype.initialize = Placeholder.prototype.placeholderHandler;
